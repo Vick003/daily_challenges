877fffd479ea066d70dbcf10774313669ee648d5
Vick <eapvic@gmail.com>
2015-10-27 20:16:20 -0700

    daily_challenges_005 - Vick Eap

--- challenge005/answer.js
+++ challenge005/answer.js
@@ -1,1 +1,15 @@
-answer.js
+
+
+function sort_object_list_by_name(list) {
+    var first, second;
+    for (var i=0; i<list.length-1; i++) {
+        if (list[i].name > list[i+1].name) {
+            first = list[i+1];
+            second = list[i];
+            list[i] = first;
+            list[i+1] = second;
+        }
+    }
+    console.log(list);
+    return list;
+}
